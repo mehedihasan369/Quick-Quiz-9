b931c652dd783fab04f9e64d7438d44c0a2d454c
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-10-12 21:55:40 +0600

    recharts

--- src/Components/Analytics/Analytics.js
+++ src/Components/Analytics/Analytics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import './Analytics.css'
 
 
@@ -15,12 +15,14 @@ const Analytics = () => {
         <div className='align-middle'>
             <h1>Here is the statistics</h1>
             <div className='w-75 '>
-            <LineChart width={350} height={200} className='chart  ' data={topics.data}>
+      <ResponsiveContainer width={'100%'} height={200}>
+      <LineChart  className='chart  m-4' data={topics.data}>
     <Line type="monotone" dataKey="total" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" />
     <XAxis dataKey="name" />
     <YAxis />
   </LineChart>
+      </ResponsiveContainer>
   </div>
         </div>
     );
